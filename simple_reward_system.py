4e254b27f9089f11de5b926040606878b41a858c
wanjikulk <shiqokamau004@gmail.com>
2023-06-27 10:10:52 +0300

    conditional statements

---
+++ simple_reward_system.py
@@ -0,0 +1,15 @@
+reward = int(input(" Please enter a random number between 1-5: "))
+
+if reward == 1:
+    print("You have won yourself 5ok!")
+elif reward == 2:
+    print("You have won yourself 100k!")
+elif reward == 3:
+    print("You have won yourself 200k!")
+elif reward == 4:
+    print("You have won yourself 300k!")
+elif reward == 5:
+    print("You have won yourself 400k!")
+else:
+    print("The input is invalid!")
+
