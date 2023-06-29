98e6bd5de968bc1d698356e2ba189a09d8a0f946
wanjikulk <shiqokamau004@gmail.com>
2023-06-29 08:46:14 +0300

    A program to calculate your bmi and tell your weight

--- bmi.py
+++ bmi.py
@@ -10,5 +10,19 @@ hsquared = h * h
 
 bmi = w/hsquared
 
-
 print(f'Your BMI is {bmi}')
+
+if bmi <= 18:
+    print("you are under weight")
+
+
+elif bmi <= 29:
+    print("You have normal weight")
+
+elif bmi <= 34:
+    print("You are overweight!")
+
+else:
+    print('You are just obese work on your health!!')
+
+
