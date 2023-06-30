3a59aac3c4ffb22d1a82c43443a68686a0adb489
wanjikulk <shiqokamau004@gmail.com>
2023-06-30 09:59:53 +0300

    Introduction to loops in python

---
+++ DataStructures/Dictonaries.py
@@ -0,0 +1,11 @@
+#has key and value pairs
+#also defined by curly brackets
+#Has order
+#You can modify data in dictonaries
+
+friends_ages = {"Roy":29, "Jaydeh":20 ,"Marie":25 }
+friends = friends_ages.copy()
+friends.update({"Weru": 13, "Mugweru": 17})
+
+print(friends_ages)
+print(friends)
--- loops/forloop.py
+++ loops/forloop.py
@@ -1,6 +1,6 @@
 friends = ['Roy', 'weru', 'Designer', 'Ann', 'kim', 'Jane', 'Rob']
 
 
-
-
+for friend in friends:
+    print(friend)
 
