402e21cf618a9c9365aeaef6ef9bbd6e5c51f490
wanjikulk <shiqokamau004@gmail.com>
2023-07-03 08:45:31 +0300

    looping over a dictonary with has a key and a value

--- loops/forloopx2.py
+++ loops/forloopx2.py
@@ -1,4 +1,4 @@
-friends_ages = {"Roy":29, "Jaydeh":20 ,"Marie":25 }
+friends_ages = {"Roy": 29, "Jaydeh": 20, "Marie": 25}
 
-for friend_age in friends_ages:
-    print({friend_age})
+for name, age in friends_ages.items():
+    print(f"{name} : {age}")
