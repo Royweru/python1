92a83e2668b8845df4b55afd66ba501559513e20
wanjikulk <shiqokamau004@gmail.com>
2023-07-04 10:21:16 +0300

    Summary of OOP

---
+++ OOP/summaryOOP.py
@@ -0,0 +1,9 @@
+#A class is like a blueprint
+#OOP is a paradigm / Way of wriring code
+#Class is like a blueprint
+#Objects - instance of a class
+
+#Inheritance
+#Polymorphism
+#Abstraction
+#Encapsulation
