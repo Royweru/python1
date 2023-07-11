010e673719ba2fa3a306c42660200a5cad8baee9
wanjikulk <shiqokamau004@gmail.com>
2023-07-11 09:52:38 +0300

    Simple abstraction functonality

---
+++ OOP/Abs.py
@@ -0,0 +1,15 @@
+class Animal:
+    def __init__(self,name):
+        self.name = name
+
+    def make_sound(self):
+        pass
+
+
+class Cat(Animal):
+    def make_sound(self):
+        print(f'meeow')
+
+
+cat1 = Cat("Tom")
+cat1.make_sound()
